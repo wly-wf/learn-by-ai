2daf95ee78aba0fef07207064e373aa9415a47f0
wly-wf <2931366539@qq.com>
2026-06-14 17:22:01 +0800

    fix: make ld.cmd wrapper portable via rustc --print sysroot

--- src-tauri/.cargo/bin/ld.cmd
+++ src-tauri/.cargo/bin/ld.cmd
@@ -1,1 +1,1 @@
-@C:\Users\Lenovo\.rustup\toolchains\stable-x86_64-pc-windows-gnu\lib\rustlib\x86_64-pc-windows-gnu\bin\rust-lld.exe -flavor ld %*
+@for /f "tokens=*" %%i in ('rustc --print sysroot') do @"%%i\..\stable-x86_64-pc-windows-gnu\lib\rustlib\x86_64-pc-windows-gnu\bin\rust-lld.exe" -flavor ld %*
